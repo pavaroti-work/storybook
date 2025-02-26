1169cc70d39c97407c25101eda2c36786f0adf49
pavaroti-work <pavaroti_worker@proton.me>
2025-02-26 11:39:17 +0100

    Add files via upload

--- stats.js
+++ stats.js
@@ -1,126 +1,112 @@
 'use strict';
 
-const Table = require('cli-table');
-const filesize = require('filesize');
 const chalk = require('chalk');
-const join = require('path').join;
-const fs = require('fs');
-const mkdirp = require('mkdirp');
+const Table = require('cli-table');
 
-const BUNDLE_SIZES_FILE_NAME = join(__dirname, '../../build/bundle-sizes.json');
-const prevBuildResults = fs.existsSync(BUNDLE_SIZES_FILE_NAME)
-  ? require(BUNDLE_SIZES_FILE_NAME)
-  : {bundleSizes: []};
+function percentChange(prev, current, prevSem, currentSem) {
+  const [mean, sd] = calculateMeanAndSdOfRatioFromDeltaMethod(
+    prev,
+    current,
+    prevSem,
+    currentSem
+  );
+  const pctChange = +(mean * 100).toFixed(1);
+  const ci95 = +(100 * 1.96 * sd).toFixed(1);
 
-const currentBuildResults = {
-  // Mutated inside build.js during a build run.
-  bundleSizes: [],
-};
-
-function saveResults() {
-  if (process.env.CI === true) {
-    mkdirp.sync('build/sizes');
-    fs.writeFileSync(
-      join('build', 'sizes', `bundle-sizes-${process.env.NODE_INDEX}.json`),
-      JSON.stringify(currentBuildResults, null, 2)
-    );
+  const ciInfo = ci95 > 0 ? ` +- ${ci95} %` : '';
+  const text = `${pctChange > 0 ? '+' : ''}${pctChange} %${ciInfo}`;
+  if (pctChange + ci95 < 0) {
+    return chalk.green(text);
+  } else if (pctChange - ci95 > 0) {
+    return chalk.red(text);
   } else {
-    // Write all the bundle sizes to a single JSON file.
-    fs.writeFileSync(
-      BUNDLE_SIZES_FILE_NAME,
-      JSON.stringify(currentBuildResults, null, 2)
-    );
+    // Statistically insignificant.
+    return text;
   }
 }
 
-function fractionalChange(prev, current) {
-  return (current - prev) / prev;
+function calculateMeanAndSdOfRatioFromDeltaMethod(
+  meanControl,
+  meanTest,
+  semControl,
+  semTest
+) {
+  const mean =
+    (meanTest - meanControl) / meanControl -
+    (Math.pow(semControl, 2) * meanTest) / Math.pow(meanControl, 3);
+  const variance =
+    Math.pow(semTest / meanControl, 2) +
+    Math.pow(semControl * meanTest, 2) / Math.pow(meanControl, 4);
+  return [mean, Math.sqrt(variance)];
 }
 
-function percentChangeString(change) {
-  if (!isFinite(change)) {
-    // When a new package is created
-    return 'n/a';
-  }
-  const formatted = (change * 100).toFixed(1);
-  if (/^-|^0(?:\.0+)$/.test(formatted)) {
-    return `${formatted}%`;
-  } else {
-    return `+${formatted}%`;
-  }
-}
+function addBenchmarkResults(table, localResults, remoteMasterResults) {
+  const benchmarks = Object.keys(
+    (localResults && localResults.benchmarks) ||
+      (remoteMasterResults && remoteMasterResults.benchmarks)
+  );
+  benchmarks.forEach(benchmark => {
+    const rowHeader = [chalk.white.bold(benchmark)];
+    if (remoteMasterResults) {
+      rowHeader.push(chalk.white.bold('Time'));
+    }
+    if (localResults) {
+      rowHeader.push(chalk.white.bold('Time'));
+    }
+    if (localResults && remoteMasterResults) {
+      rowHeader.push(chalk.white.bold('Diff'));
+    }
+    table.push(rowHeader);
 
-const resultsHeaders = [
-  'Bundle',
-  'Prev Size',
-  'Current Size',
-  'Diff',
-  'Prev Gzip',
-  'Current Gzip',
-  'Diff',
-];
-
-function generateResultsArray(current, prevResults) {
-  return current.bundleSizes
-    .map(result => {
-      const prev = prevResults.bundleSizes.filter(
-        res =>
-          res.filename === result.filename &&
-          res.bundleType === result.bundleType
-      )[0];
-      if (result === prev) {
-        // We didn't rebuild this bundle.
-        return;
+    const measurements =
+      (localResults && localResults.benchmarks[benchmark].averages) ||
+      (remoteMasterResults &&
+        remoteMasterResults.benchmarks[benchmark].averages);
+    measurements.forEach((measurement, i) => {
+      const row = [chalk.gray(measurement.entry)];
+      let remoteMean;
+      let remoteSem;
+      if (remoteMasterResults) {
+        remoteMean = remoteMasterResults.benchmarks[benchmark].averages[i].mean;
+        remoteSem = remoteMasterResults.benchmarks[benchmark].averages[i].sem;
+        // https://en.wikipedia.org/wiki/1.96 gives a 99% confidence interval.
+        const ci95 = remoteSem * 1.96;
+        row.push(
+          chalk.white(+remoteMean.toFixed(2) + ' ms +- ' + ci95.toFixed(2))
+        );
       }
-
-      const size = result.size;
-      const gzip = result.gzip;
-      let prevSize = prev ? prev.size : 0;
-      let prevGzip = prev ? prev.gzip : 0;
-
-      return {
-        filename: result.filename,
-        bundleType: result.bundleType,
-        packageName: result.packageName,
-        prevSize: filesize(prevSize),
-        prevFileSize: filesize(size),
-        prevFileSizeChange: fractionalChange(prevSize, size),
-        prevFileSizeAbsoluteChange: size - prevSize,
-        prevGzip: filesize(prevGzip),
-        prevGzipSize: filesize(gzip),
-        prevGzipSizeChange: fractionalChange(prevGzip, gzip),
-        prevGzipSizeAbsoluteChange: gzip - prevGzip,
-      };
-      // Strip any nulls
-    })
-    .filter(f => f);
+      let localMean;
+      let localSem;
+      if (localResults) {
+        localMean = localResults.benchmarks[benchmark].averages[i].mean;
+        localSem = localResults.benchmarks[benchmark].averages[i].sem;
+        const ci95 = localSem * 1.96;
+        row.push(
+          chalk.white(+localMean.toFixed(2) + ' ms +- ' + ci95.toFixed(2))
+        );
+      }
+      if (localResults && remoteMasterResults) {
+        row.push(percentChange(remoteMean, localMean, remoteSem, localSem));
+      }
+      table.push(row);
+    });
+  });
 }
 
-function printResults() {
-  const table = new Table({
-    head: resultsHeaders.map(label => chalk.gray.yellow(label)),
-  });
-
-  const results = generateResultsArray(currentBuildResults, prevBuildResults);
-  results.forEach(result => {
-    table.push([
-      chalk.white.bold(`${result.filename}  (${result.bundleType})`),
-      chalk.gray.bold(result.prevSize),
-      chalk.white.bold(result.prevFileSize),
-      percentChangeString(result.prevFileSizeChange),
-      chalk.gray.bold(result.prevGzip),
-      chalk.white.bold(result.prevGzipSize),
-      percentChangeString(result.prevGzipSizeChange),
-    ]);
-  });
-
-  return table.toString();
+function printResults(localResults, remoteMasterResults) {
+  const head = [''];
+  if (remoteMasterResults) {
+    head.push(chalk.yellow.bold('Remote (Merge Base)'));
+  }
+  if (localResults) {
+    head.push(chalk.green.bold('Local (Current Branch)'));
+  }
+  if (localResults && remoteMasterResults) {
+    head.push('');
+  }
+  const table = new Table({head});
+  addBenchmarkResults(table, localResults, remoteMasterResults);
+  console.log(table.toString());
 }
 
-module.exports = {
-  currentBuildResults,
-  generateResultsArray,
-  printResults,
-  saveResults,
-  resultsHeaders,
-};
+module.exports = printResults;
